a5f59c251ea361fc7fd4bf0fe82dee73541ae8dd
EngTamim <eng.tamim83@gmail.com>
2023-09-07 14:11:16 +0300

    Complete the Tasks of Session3_4

---
+++ Session3_4_Python_Tasks/Task3.py
@@ -0,0 +1,6 @@
+# Task 3
+# Write Python code to get the ASCII value of a character
+
+userChar = input("Enter your char: ")
+asciiValue = ord(userChar)
+print(f"The ASCII value of '{userChar}' is {asciiValue}")
