59b93c2d0f3ad280e2d8606ca5b3a0319f2e7e6e
Tamim Alamir <103435431+EngTamim@users.noreply.github.com>
2023-10-06 17:16:31 +0300

    Delete Session3_4_Python_Tasks directory

--- Session3_4_Python_Tasks/QuickTask3.py
+++
@@ -1,8 +0,0 @@
-# Quick Task3
-# Write a python code to write a "list" to a file
-
-color = ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
-
-with open('TextFile.txt', 'w') as myfile:
-    for item in color:
-        myfile.write(str(item) + '\n')
--- Session3_4_Python_Tasks/header_file.h
+++
@@ -1,5 +0,0 @@
-void MovPro1(void);
-void MovPro2(void);
-void Beep(unsigned int Duration);
-unsigned int ReadSensors(void);
-unsigned int AdjustTheDoor(unsigned int Door);
